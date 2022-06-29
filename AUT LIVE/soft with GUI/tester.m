26f06adb9903b361d77c48f533108856a8827195
ryuoda123 <81938615+ryuoda123@users.noreply.github.com>
2022-06-29 15:58:19 +1200

    Add files via upload

--- AUT LIVE/soft with GUI/tester.m
+++ AUT LIVE/soft with GUI/tester.m
@@ -1,6 +1,15 @@
+% %%
+% a = cap_detection(0.9, 0.05, 65);
+% dia = a.obtainDiameter(imread("sampleFig.png"))
+% %%
+% b = ["-81.3", "-293.45"];
+% disp(b)
+
 %%
-a = cap_detection(0.9, 0.05, 65);
-dia = a.obtainDiameter(imread("sampleFig.png"))
-%%
-b = ["-81.3", "-293.45"];
-disp(b)
+img = imread("normal.png");
+img  = img(:,width(img)/2:width(img),:);
+% gray = rgb2gray(img);
+imshow(img)
+
+% fig = figure;
+% imsohw(img)
