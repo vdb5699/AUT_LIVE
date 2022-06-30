2140e80b43ace7da6bb32b155904e7e7e9956ff0
ryuoda123 <81938615+ryuoda123@users.noreply.github.com>
2022-07-01 11:26:03 +1200

    Add files via upload

--- AUT LIVE/soft with GUI/convert_coord.m
+++ AUT LIVE/soft with GUI/convert_coord.m
@@ -1,11 +1,13 @@
 classdef convert_coord
     methods
         function [newX, newY]= convertBrown(obj, x, y)
-            newX = 0.0319*x - 34.986;
-            newY = -0.0318*y + 18.779;
-
+%             newX = 0.0319*x - 34.986;
+%             newY = -0.0318*y + 18.779;
+            newX = (1e-9*(x^3)) - (4e-6*(x^2)) + 0.0354*x - 35.343;
+            newY = (1e-9*(y^3)) + (3e-6*(x^2)) -0.0325*x + 17.602;
+            newX = newX*10;
+            newY = newY*10;
             newCoord = obj.convertDirection(newX, newY, ((3*pi)/4));
-            newCoord = [newCoord(1)*10, newCoord(2)*10];
             newX = newCoord(1);
             newY = newCoord(2);
             return
