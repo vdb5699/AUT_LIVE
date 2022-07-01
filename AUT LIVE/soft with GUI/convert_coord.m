982ba09a804f487e17e983cc68493bfd454eb026
ryuoda123 <81938615+ryuoda123@users.noreply.github.com>
2022-07-01 14:17:17 +1200

    Add files via upload

--- AUT LIVE/soft with GUI/convert_coord.m
+++ AUT LIVE/soft with GUI/convert_coord.m
@@ -4,7 +4,7 @@ classdef convert_coord
 %             newX = 0.0319*x - 34.986;
 %             newY = -0.0318*y + 18.779;
             newX = (1e-9*(x^3)) - (4e-6*(x^2)) + 0.0354*x - 35.343;
-            newY = (1e-9*(y^3)) + (3e-6*(y^2)) -0.0325*y + 17.602;
+            newY = -1*(1e-9*(y^3)) + (3e-6*(y^2)) -0.0325*y + 17.602;
             newX = newX*10;
             newY = newY*10;
             newCoord = obj.convertDirection(newX, newY, ((3*pi)/4));
