dfee0590296278362aaee2297f6436d9761ac6e2
Ryuichi Odani <ssz9518@autuni.ac.nz>
2022-07-04 09:57:25 +1200

    Update tester2.m

--- AUT LIVE/soft with GUI/tester2.m
+++ AUT LIVE/soft with GUI/tester2.m
@@ -5,18 +5,22 @@ fig = figure;
 camera = camera.setVars("4416x1242", 4,4,4,4);
 imshow(camera.manualImageAcq());
 
-%% analyse first coord
+%% analyse 1st coord
 converter = convert_coord();
 coord = []; %add coord
-cap1 = cap(coord, "Brown");
-[newCamPosX newCamPosY] = converter.convertBrown(cap1.centreCoord(1), cap1.centreCoord(2));
+% cap1 = cap(coord, "Brown");
+cap1 = cap(coord, "Red");
+% [newCamPosX newCamPosY] = converter.convertBrown(cap1.centreCoord(1), cap1.centreCoord(2));
+[newCamPosX newCamPosY] = converter.convertRed(cap1.centreCoord(1), cap1.centreCoord(2));
 newCamPosX = newCamPosX + 326.9438
 newCamPosY = newCamPosY + -581.9982
 
-%% analyse second coord
+%% analyse 2nd coord
 coord = []; %add coord
-cap2 = cap(coord, "Brown");
-[posX, posY] = converter.convertBrown(cap2.centreCoord(1), cap2.centreCoord(2));
+% cap2 = cap(coord, "Brown");
+cap2 = cap(coord, "Red");
+% [posX, posY] = converter.convertBrown(cap2.centreCoord(1), cap2.centreCoord(2));
+[posX, posY] = converter.convertRed(cap2.centreCoord(1), cap2.centreCoord(2));
 camToGrip = converter.convertDirection(100.3,160.625, (3*pi)/4);
 posX = newCamPosx + camToGrip(1) + posX
 posY = newCamPosY + camToGrip(2) + posY
