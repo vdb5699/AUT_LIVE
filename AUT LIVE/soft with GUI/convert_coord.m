5d4760f61341547d774adcbff2bafdcec2f961b0
Ryuichi Odani <ssz9518@autuni.ac.nz>
2022-07-04 12:19:59 +1200

    Update convert_coord.m

--- AUT LIVE/soft with GUI/convert_coord.m
+++ AUT LIVE/soft with GUI/convert_coord.m
@@ -16,10 +16,12 @@ classdef convert_coord
         function [newX, newY] = convertRed(obj, x, y)
 %             newX = 0.0368*x - 40.511;
 %             newY = -0.0368*y + 22.257;
-            newX = (4e-9*(x^3)) - (1e-6*(x^2)) + 0.0379*x - 40.481;
-            newY = -1*(4e-9*(y^3)) + (8e-6*(y^2)) - 0.0412*y +21.453; 
-            newX = newX*10;
-            newY = newY*10;
+%             newX = (4e-9*(x^3)) - (1e-6*(x^2)) + 0.0379*x - 40.481;
+%             newY = -1*(4e-9*(y^3)) + (8e-6*(y^2)) - 0.0412*y +21.453; 
+            newX = (1e-9*(x^3)) - (4e-6*(x^2)) + 0.0354*x - 35.343;
+            newY = -1*(1e-9*(y^3)) + (3e-6*(y^2)) -0.0325*y + 17.602;
+            newX = newX*10*1.225;
+            newY = newY*10*1.225;
             newCoord = obj.convertDirection(newX, newY, ((3*pi)/4));
             newX = newCoord(1);
             newY = newCoord(2);
