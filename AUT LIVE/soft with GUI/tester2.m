c9619bf8f655180fc80ab82caa37a690a00956cc
Nikhil Prasad <nikhil.p0936@gmail.com>
2022-07-04 15:56:37 +1200

    Changed X values by 5mm

--- AUT LIVE/soft with GUI/tester2.m
+++ AUT LIVE/soft with GUI/tester2.m
@@ -7,7 +7,7 @@ imshow(camera.manualImageAcq());
 
 %% analyse 1st coord
 converter = convert_coord();
-coord = [1427 505]; %add coord
+coord = [307 85]; %add coord
 % cap1 = cap(coord, "Brown");
 cap1 = cap(coord, "Red");
 % [newCamPosX newCamPosY] = converter.convertBrown(cap1.centreCoord(1), cap1.centreCoord(2));
@@ -16,11 +16,12 @@ newCamPosX = newCamPosX + 326.9438
 newCamPosY = newCamPosY + -581.9982
 
 %% analyse 2nd coord
-coord = [1135 564]; %add coord
+coord = [1140 547]; %add coord
 % cap2 = cap(coord, "Brown");
 cap2 = cap(coord, "Red");
 % [posX, posY] = converter.convertBrown(cap2.centreCoord(1), cap2.centreCoord(2));
 [posX, posY] = converter.convertRed(cap2.centreCoord(1), cap2.centreCoord(2));
-camToGrip = converter.convertDirection(100.3,160.625, (3*pi)/4);
+% camToGrip = converter.convertDirection(108.3,160.625, (3*pi)/4);
+camToGrip = converter.convertDirection(105.3,160.625, (3*pi)/4);
 posX = newCamPosX + camToGrip(1) + posX
 posY = newCamPosY + camToGrip(2) + posY
