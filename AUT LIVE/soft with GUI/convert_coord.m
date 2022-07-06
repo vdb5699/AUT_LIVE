3882ec7b82850834d970abf1fd5152ff90574561
ryuoda123 <odani.ryuichi@gmail.com>
2022-07-07 11:55:10 +1200

    updated calibration

--- AUT LIVE/soft with GUI/convert_coord.m
+++ AUT LIVE/soft with GUI/convert_coord.m
@@ -1,10 +1,10 @@
 classdef convert_coord
     methods
         function [newX, newY]= convertBrown(obj, x, y)
-%             newX = 0.0319*x - 34.986;
-%             newY = -0.0318*y + 18.779;
-            newX = (1e-9*(x^3)) - (4e-6*(x^2)) + 0.0354*x - 35.343;
-            newY = -1*(1e-9*(y^3)) + (3e-6*(y^2)) -0.0325*y + 17.602;
+%             newX = (1e-9*(x^3)) - (4e-6*(x^2)) + 0.0354*x - 35.343;
+%             newY = -1*(1e-9*(y^3)) + (3e-6*(y^2)) -0.0325*y + 17.602;
+            newX = -1*(1e06*(x*x)) + (0.0339*x) - 30.833;
+            newY = -1*(9e-9*(y*y)) - 0.03*y +19.819;
             newX = newX*10;
             newY = newY*10;
             newCoord = obj.convertDirection(newX, newY, ((3*pi)/4));
